0c010a7f287440480eea44da1950c0af2543ad4d
X-Doctor-X <57560155+X-Doctor-X@users.noreply.github.com>
2019-11-09 07:49:06 -0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const generator = require('generate-password');
+
+client.on('ready', () => {
+ client.user.setActivity("Violando personas :)");
+ console.log("Tu bot ("+client.user.tag+") ahora se encuentra preparado!");
+ });
+ 
+client.on("message", async message => {
+  const prefix = "-";
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  if(command === "help"){
+	var i = 0;
+    while (i<1000) { //Number of messages
+    var password = generator.generate({
+    length: 16,
+    numbers: true
+});
+	message.channel.send("https://discord.gift/" + password)
+    i++;
+	}
+  }
+})
+
+client.login('INSERT_YOUR_TOKEN');
